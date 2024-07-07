46d8d558faef44cc35d6e3f9ab4222ba7b4c3d6f
Luciano Figueroa <lumusitech@gmail.com>
2024-07-07 12:52:22 -0300

    feat: add pokemon schema

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,18 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Pokemon {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  nro: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
