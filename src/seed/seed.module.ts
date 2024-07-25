1a92f78c64b52da293a6242e834fa52b5f130000
Luciano Figueroa <lumusitech@gmail.com>
2024-07-24 23:28:51 -0300

    feat: recreate error with dependency injection nest and axios 3.0.2

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SeedController } from './seed.controller';
 import { SeedService } from './seed.service';
@@ -5,5 +6,6 @@ import { SeedService } from './seed.service';
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [HttpModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,10 @@
+import type { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly httpService: HttpService) {}
+
   async executeSeed() {
     return 'Seed executed';
   }
