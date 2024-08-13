454cb5fc40d758bbad7508890718248dd88477b4
Luciano Figueroa <lumusitech@gmail.com>
2024-08-13 13:47:32 -0300

    chore: add app config method for env vars

---
+++ src/config/app.config.ts
@@ -0,0 +1,7 @@
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGO_DB_URL,
+  port: +process.env.PORT || 3001,
+  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  defaultOffset: process.env.DEFAULT_OFFSET || 0,
+});
